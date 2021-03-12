0d58064ce919558e47e53a0ea04934dc8ce8442f
huuhan2507@gmail.com <you@example.com>
2021-03-13 01:57:19 +0700

    update

--- Module4/SS15_Authorization/exercise/authorization_exercise/src/main/java/com/example/config/WebSecurityConfig.java
+++ Module4/SS15_Authorization/exercise/authorization_exercise/src/main/java/com/example/config/WebSecurityConfig.java
@@ -36,16 +36,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-
-        // Các trang không yêu cầu login
-        http.authorizeRequests().antMatchers("/blog/view_by_blog/*","/blog/sort","/","/blog/search","/logoutSuccessful","/blog/view/*", "/login").permitAll();
-
+        http.authorizeRequests().antMatchers( "/","/login" ).permitAll();
         // Trang /userInfo yêu cầu phải login với vai trò ROLE_USER hoặc ROLE_ADMIN.
         // Nếu chưa login, nó sẽ redirect tới trang /login.
-        http.authorizeRequests().antMatchers("/blog/create").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+        http.authorizeRequests().antMatchers("/user").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
 
         // Trang chỉ dành cho ADMIN
-        http.authorizeRequests().antMatchers("/blog/edit/*","/blog/delete/*","/category", "/category/*").access("hasRole('ROLE_ADMIN')");
+        http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
 
         // Khi người dùng đã login, với vai trò XX.
         // Nhưng truy cập vào trang yêu cầu vai trò YY,
@@ -57,12 +54,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // Submit URL của trang login
                 .loginProcessingUrl("/j_spring_security_check") // Submit URL
                 .loginPage("/login")//
-                .defaultSuccessUrl("/home")//
+                .defaultSuccessUrl("/")//
                 .failureUrl("/login?error=true")//
                 .usernameParameter("username")//
                 .passwordParameter("password")
                 // Cấu hình cho Logout Page.
-                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
 
         // Cấu hình Remember Me.
         http.authorizeRequests().and() //
