c6a77c5c88176f2c882d87e131b764cd255c96c0
huuhan2507@gmail.com <you@example.com>
2021-04-02 23:06:39 +0700

    update

--- Module5/Angular/src/app/model/StudentDao.ts
+++ Module5/Angular/src/app/model/StudentDao.ts
@@ -5,6 +5,7 @@ export const studentDao: IStudent[] = [
   {id: 2, name: 'Nguyễn Long Phin', age: 20, mark: 4, image: ''},
   {id: 3, name: 'Lê Quang Dương', age: 23, mark: 5, image: ''},
   {id: 4, name: 'Nguyễn Việt Dũng', age: 28, mark: 7, image: ''},
-  {id: 5, name: 'Nguyễn Trường Phúc', age: 29, mark: 4, image: ''},
-  {id: 6, name: 'Phạm Hồng Sơn', age: 25, mark: 6, image: ''}
+  {id: 5, name: 'Phạm Trường Phúc', age: 29, mark: 4, image: ''},
+  {id: 6, name: 'Phạm Hồng Sơn', age: 25, mark: 6, image: ''},
+  {id: 7, name: 'Huỳnh Ngọc Thịnh', age: 25, mark: 3, image: ''}
 ];
